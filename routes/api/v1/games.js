faafc5680c5fcfd082db47996e8fbf90c798ab3e
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-08-10 14:30:19 -0600

    Adds delete and update routes

--- routes/api/v1/games.js
+++ routes/api/v1/games.js
@@ -20,6 +20,47 @@ router.post("/", function(req, res, next) {
     });
 });
 
+router.put('/:id', function(req, res, next) {
+  Game.update(
+    {
+      title: req.body.title,
+      price: req.body.price,
+      releaseYear: req.body.releaseYear,
+      active: req.body.active
+    },
+    {
+      returning: true,
+      where: {
+        id: parseInt(req.params.id)
+      }
+    }
+  )
+  .then(([rowsUpdate, [updatedGame]]) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(202).send(JSON.stringify(updatedGame));
+  })
+  .catch(error => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(500).send({ error });
+  });
+});
+
+router.delete('/:id', function(req, res, next) {
+  Game.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(game => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(204).send(JSON.stringify(game));
+  })
+  .catch(error => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(500).send({ error });
+  })
+})
+
 router.get('/', function(req, res, next) {
   Game.findAll()
   .then(games => {
